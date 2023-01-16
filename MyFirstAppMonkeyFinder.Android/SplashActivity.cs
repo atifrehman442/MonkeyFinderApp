77c540409fcd61a75e7f02c0ebc5d701d475e293
atifrehman442 <78613062+atifrehman442@users.noreply.github.com>
2023-01-16 14:48:09 +0500

    Add files via upload

---
+++ MyFirstAppMonkeyFinder.Android/SplashActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace MyFirstAppMonkeyFinder.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash",
+        MainLauncher = true,
+        NoHistory = true,
+        Icon = "@drawable/splash")]
+
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Timer timer = new Timer();
+            timer.Interval = 10;
+            timer.AutoReset = false; // Do not reset the timer after it's elapsed
+            timer.Elapsed += (object sender, ElapsedEventArgs e) =>
+            {
+                StartActivity(typeof(MainActivity));
+            };
+            timer.Start();
+            // Create your application here
+        }
+    }
+}
